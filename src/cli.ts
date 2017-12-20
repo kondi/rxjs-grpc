20f543e775528141b643fde96c9e7c7b102b8496
Michael Mrowetz <mmrowetz@gaikai.com>
2017-12-20 14:30:42 +0100

    import RxJS in it's ideomatic way

--- src/cli.ts
+++ src/cli.ts
@@ -170,7 +170,7 @@ function transformTypeScriptSource(source: string) {
   // Remove imports
   source = source.replace(/^import.*?$\n?/mg, '');
   // Add our imports
-  source = `import { Observable } from 'rxjs';\n${source}`;
+  source = `import { Observable } from 'rxjs/Rx';\n${source}`;
 
   if(source.includes("$protobuf")) {
     source = `import * as $protobuf from 'protobufjs';\n${source}`
