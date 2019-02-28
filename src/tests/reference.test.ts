a6d7ac0d7f6c3a38c4b260b554bd688adf9a95b3
Pusztai Tibor <kondi.elte@gmail.com>
2019-02-28 15:39:54 +0100

    Add new unit test for message field reference

---
+++ src/tests/reference.test.ts
@@ -0,0 +1,61 @@
+import * as cli from '../cli';
+import { compileInMemory } from './utils';
+
+describe('reference test', () => {
+
+  let namespaces: string;
+
+  beforeAll(async () => {
+    namespaces = await cli.buildTypeScriptFromSources([
+      `
+        syntax = "proto3";
+        package test;
+
+        message Message {
+          SubMessage sub = 1;
+        }
+
+        message SubMessage {
+          string subName = 1;
+        }
+      `
+    ]);
+  });
+
+  it('should work with null', async () => {
+    const result = compileInMemory({
+      'grpc-namespaces.ts': namespaces,
+      'test.ts': `
+        import { test } from './grpc-namespaces';
+
+        const message: test.Message = {
+          sub: null
+        };
+
+        console.log(message.sub);
+      `
+    });
+    expect(result.errors).toEqual([]);
+    expect(result.ok).toBe(true);
+  });
+
+  it('should work with submessage', async () => {
+    const result = compileInMemory({
+      'grpc-namespaces.ts': namespaces,
+      'test.ts': `
+        import { test } from './grpc-namespaces';
+
+        const message: test.Message = {
+          sub: {
+            subName: 'subNameValue'
+          }
+        };
+
+        console.log(message.sub, message.sub.subName);
+      `
+    });
+    expect(result.errors).toEqual([]);
+    expect(result.ok).toBe(true);
+  });
+
+});
