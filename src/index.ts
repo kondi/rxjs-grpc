3c50d39435e1e4ed4cd7fecf2038b02016a36522
Pusztai Tibor <kondi.elte@gmail.com>
2019-03-05 15:45:57 +0100

    Extract generic functions from index.ts to utils.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import * as protoLoader from '@grpc/proto-loader';
 import * as grpc from 'grpc';
-import { Observable } from 'rxjs';
 
-import { lookupPackage } from './utils';
-
-type DynamicMethods = { [name: string]: any };
-type GrpcService<T> = typeof grpc.Client & {
-  service: grpc.ServiceDefinition<T>;
-};
+import {
+  ClientFactoryConstructor,
+  createService,
+  createServiceClient,
+  DynamicMethods,
+  getServiceNames,
+  grpcLoad,
+  GrpcService,
+  lookupPackage,
+} from './utils';
 
 export interface GenericServerBuilder<T> {
   start(address: string, credentials?: grpc.ServerCredentials): void;
@@ -41,63 +43,6 @@ export function serverBuilder<T>(
   return builder as any;
 }
 
-function grpcLoad(protoPath: string) {
-  const packageDefinition = protoLoader.loadSync(protoPath, {
-    keepCase: true,
-    defaults: true,
-    oneofs: true,
-  });
-  return grpc.loadPackageDefinition(packageDefinition);
-}
-
-function createService(Service: GrpcService<any>, rxImpl: DynamicMethods) {
-  const service: DynamicMethods = {};
-  for (const name in Service.prototype) {
-    if (typeof rxImpl[name] === 'function') {
-      service[name] = createMethod(rxImpl, name, Service.prototype);
-    }
-  }
-  return service;
-}
-
-function createMethod(rxImpl: DynamicMethods, name: string, serviceMethods: DynamicMethods) {
-  return serviceMethods[name].responseStream
-    ? createStreamingMethod(rxImpl, name)
-    : createUnaryMethod(rxImpl, name);
-}
-
-function createUnaryMethod(rxImpl: DynamicMethods, name: string): grpc.handleUnaryCall<any, any> {
-  return function(call, callback) {
-    try {
-      const response: Observable<any> = rxImpl[name](call.request, call.metadata);
-      response.subscribe(data => callback(null, data), error => callback(error, null));
-    } catch (error) {
-      callback(error, null);
-    }
-  };
-}
-
-function createStreamingMethod(
-  rxImpl: DynamicMethods,
-  name: string,
-): grpc.handleServerStreamingCall<any, any> {
-  return async function(call) {
-    try {
-      const response: Observable<any> = rxImpl[name](call.request, call.metadata);
-      await response.forEach(data => call.write(data));
-    } catch (error) {
-      call.emit('error', error);
-    }
-    call.end();
-  };
-}
-
-export type ClientFactoryConstructor<T> = new (
-  address: string,
-  credentials?: grpc.ChannelCredentials,
-  options?: any,
-) => T;
-
 export function clientFactory<T>(protoPath: string, packageName: string) {
   class Constructor {
     readonly __args: [string, grpc.ChannelCredentials, any | undefined];
@@ -115,52 +60,4 @@ export function clientFactory<T>(protoPath: string, packageName: string) {
   }
 
   return (Constructor as any) as ClientFactoryConstructor<T>;
-}
-
-function getServiceNames(pkg: grpc.GrpcObject) {
-  return Object.keys(pkg).filter(name => (pkg[name] as GrpcService<any>).service);
-}
-
-function createServiceClient(
-  GrpcClient: GrpcService<any>,
-  args: [string, grpc.ChannelCredentials, any | undefined],
-) {
-  const grpcClient = new GrpcClient(args[0], args[1], args[2]);
-  const rxClient: DynamicMethods = {};
-  for (const name of Object.keys(GrpcClient.prototype)) {
-    rxClient[name] = createClientMethod(grpcClient, name);
-  }
-  return rxClient;
-}
-
-function createClientMethod(grpcClient: DynamicMethods, name: string) {
-  return grpcClient[name].responseStream
-    ? createStreamingClientMethod(grpcClient, name)
-    : createUnaryClientMethod(grpcClient, name);
-}
-
-function createUnaryClientMethod(grpcClient: DynamicMethods, name: string) {
-  return function(...args: any[]) {
-    return new Observable(observer => {
-      grpcClient[name](...args, (error: any, data: any) => {
-        if (error) {
-          observer.error(error);
-        } else {
-          observer.next(data);
-        }
-        observer.complete();
-      });
-    });
-  };
-}
-
-function createStreamingClientMethod(grpcClient: DynamicMethods, name: string) {
-  return function(...args: any[]) {
-    return new Observable(observer => {
-      const call = grpcClient[name](...args);
-      call.on('data', (data: any) => observer.next(data));
-      call.on('error', (error: any) => observer.error(error));
-      call.on('end', () => observer.complete());
-    });
-  };
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,26 @@
-import { GrpcObject } from 'grpc';
+import * as protoLoader from '@grpc/proto-loader';
+import {
+  ChannelCredentials,
+  Client,
+  GrpcObject,
+  handleServerStreamingCall,
+  handleUnaryCall,
+  loadPackageDefinition,
+  ServiceDefinition,
+} from 'grpc';
+import { Observable } from 'rxjs';
+
+export type ClientFactoryConstructor<T> = new (
+  address: string,
+  credentials?: ChannelCredentials,
+  options?: any,
+) => T;
+
+export type DynamicMethods = { [name: string]: any };
+
+export type GrpcService<T> = typeof Client & {
+  service: ServiceDefinition<T>;
+};
 
 export function lookupPackage(root: GrpcObject, packageName: string) {
   let pkg = root;
@@ -6,4 +28,103 @@ export function lookupPackage(root: GrpcObject, packageName: string) {
     pkg = pkg[name] as GrpcObject;
   }
   return pkg;
+}
+
+export function grpcLoad(protoPath: string) {
+  const packageDefinition = protoLoader.loadSync(protoPath, {
+    keepCase: true,
+    defaults: true,
+    oneofs: true,
+  });
+  return loadPackageDefinition(packageDefinition);
+}
+
+export function createService(Service: GrpcService<any>, rxImpl: DynamicMethods) {
+  const service: DynamicMethods = {};
+  for (const name in Service.prototype) {
+    if (typeof rxImpl[name] === 'function') {
+      service[name] = createMethod(rxImpl, name, Service.prototype);
+    }
+  }
+  return service;
+}
+
+function createMethod(rxImpl: DynamicMethods, name: string, serviceMethods: DynamicMethods) {
+  return serviceMethods[name].responseStream
+    ? createStreamingMethod(rxImpl, name)
+    : createUnaryMethod(rxImpl, name);
+}
+
+function createUnaryMethod(rxImpl: DynamicMethods, name: string): handleUnaryCall<any, any> {
+  return function(call, callback) {
+    try {
+      const response: Observable<any> = rxImpl[name](call.request, call.metadata);
+      response.subscribe(data => callback(null, data), error => callback(error, null));
+    } catch (error) {
+      callback(error, null);
+    }
+  };
+}
+
+function createStreamingMethod(
+  rxImpl: DynamicMethods,
+  name: string,
+): handleServerStreamingCall<any, any> {
+  return async function(call) {
+    try {
+      const response: Observable<any> = rxImpl[name](call.request, call.metadata);
+      await response.forEach(data => call.write(data));
+    } catch (error) {
+      call.emit('error', error);
+    }
+    call.end();
+  };
+}
+
+export function getServiceNames(pkg: GrpcObject) {
+  return Object.keys(pkg).filter(name => (pkg[name] as GrpcService<any>).service);
+}
+
+export function createServiceClient(
+  GrpcClient: GrpcService<any>,
+  args: [string, ChannelCredentials, any | undefined],
+) {
+  const grpcClient = new GrpcClient(args[0], args[1], args[2]);
+  const rxClient: DynamicMethods = {};
+  for (const name of Object.keys(GrpcClient.prototype)) {
+    rxClient[name] = createClientMethod(grpcClient, name);
+  }
+  return rxClient;
+}
+
+function createClientMethod(grpcClient: DynamicMethods, name: string) {
+  return grpcClient[name].responseStream
+    ? createStreamingClientMethod(grpcClient, name)
+    : createUnaryClientMethod(grpcClient, name);
+}
+
+function createUnaryClientMethod(grpcClient: DynamicMethods, name: string) {
+  return function(...args: any[]) {
+    return new Observable(observer => {
+      grpcClient[name](...args, (error: any, data: any) => {
+        if (error) {
+          observer.error(error);
+        } else {
+          observer.next(data);
+        }
+        observer.complete();
+      });
+    });
+  };
+}
+
+function createStreamingClientMethod(grpcClient: DynamicMethods, name: string) {
+  return function(...args: any[]) {
+    return new Observable(observer => {
+      const call = grpcClient[name](...args);
+      call.on('data', (data: any) => observer.next(data));
+      call.on('error', (error: any) => observer.error(error));
+      call.on('end', () => observer.complete());
+    });
+  };
 }
